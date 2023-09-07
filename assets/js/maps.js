b5ae1fc52a300f4d8b409b8e4651d45dbe498654
JIBIN JAIN JOHNY <jibinjjohny11@gmail.com>
2023-09-07 11:47:01 +0200

    Update map code

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,36 +1,30 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: {
-            lat: 46.619261,
-            lng: -33.134766
-        }
+        zoom: 2,
+        center: new google.maps.LatLng(46.619261, -33.134766)
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [{
-            lat: 40.785091,
-            lng: -73.968285
+            position: new google.maps.LatLng(40.785091, -73.968285)
         },
         {
-            lat: 41.084045,
-            lng: -73.874245
+            position: new google.maps.LatLng(41.084045, -73.874245)
+
         },
         {
-            lat: 40.754932,
-            lng: -73.984016
+            position: new google.maps.LatLng(40.754932, -73.984016)
         }
     ];
 
-    var markers = locations.map(function (location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
+    // Create markers.
+    for (let i = 0; i < locations.length; i++) {
+        const marker = new google.maps.Marker({
+            position: locations[i].position,
+            map: map,
         });
-    });
+    }
 
-    var markerCluster = new MarkerClusterer(map, markers, {
-        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-    });
 }
+window.initMap = initMap;
